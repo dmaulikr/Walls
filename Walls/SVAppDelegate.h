2a22ea0f07a55613d0eafc425e4d7d7b80992621
Sebastien Villar <me@sebastienvillar.com>
2014-01-18 12:05:19 +0100

    Initial Commit

---
+++ Walls/SVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVAppDelegate.h
+//  Walls
+//
+//  Created by Sebastien Villar on 18/01/14.
+//  Copyright (c) 2014 Sebastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Walls/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Walls
+//
+//  Created by Sebastien Villar on 18/01/14.
+//  Copyright (c) 2014 Sebastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+    }
+}
